5e5e5c9e11bfd69529da7e3dbde8ef7d0c67611a
huyson-pc <huysonlam@gmail.com>
2020-05-22 10:48:19 -0700

    first commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,257 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    private bool welcomeComplete = false;
+
+    private AdsManager adsManager;
+    [SerializeField] private GameObject welcomeSequence;
+    [SerializeField] private GameObject mainGame;
+
+    private string username;
+    [SerializeField] TMP_InputField loginField;
+
+    //[SerializeField] private Text goldText;
+    [SerializeField] private Button button1;
+    [SerializeField] private Button button2;
+
+    //0. Title
+    //1. welcome screen (message)
+    //a. ad
+    //2. give coins (message)
+    //3. create username
+    //4. create password
+    //5. congratulations (message)
+    //8. ad
+    //9. get into main game
+    private int welcomeIndex = 0;
+    [SerializeField] private GameObject titlePanel;
+    [SerializeField] private GameObject messagePanel;
+    [SerializeField] private GameObject loginPanel;
+
+    private int coins = 0;
+    [SerializeField] private GameObject coinsText;
+
+    void Start() {
+        adsManager = GetComponent<AdsManager>();
+        adsManager.SetAdCompleteHandler(OnAdCompleted); 
+        coinsText.GetComponent<TMP_Text>().SetText(coins.ToString());
+
+        if (welcomeComplete) {
+            StartMainGame();
+        } else {
+            welcomeSequence.SetActive(true);
+            mainGame.SetActive(false);
+        }
+    }
+
+    void OnGUI() {
+        HandleText();
+    }
+
+    private void StartMainGame() {
+        welcomeComplete = true;
+        mainGame.SetActive(true);
+        welcomeSequence.SetActive(false);
+        mainGame.GetComponent<MainGame>().InitMainGame(username, coins);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began) {
+                HandleInput();
+            }
+        } else if (Input.GetMouseButtonDown(0)) {
+            HandleInput();
+        }
+    }
+
+    private void HandleText(){
+        switch (welcomeIndex) {
+            case 0:
+                ShowTitle();
+                coinsText.SetActive(false);
+                break;
+            case 1:
+                ShowMessage("We see you are a 1st time user. Please enjoy this ad to activate ADHERO PREMIUM");
+                coinsText.SetActive(false);
+                break;
+            case 2:
+                ShowMessage("Here are some coins to start you off");
+                coinsText.SetActive(true);
+                break;
+            case 3:
+                ShowLogin("Username cost based on whatever");
+                if (loginField.interactable) {
+                    button1.GetComponentInChildren<TMP_Text>().SetText("Confirm username\n({0}g)", GetUserNameCost(loginField.text));
+                }
+                break;
+            case 4:
+                ShowLogin("Password cost based on whoever");
+                if (loginField.interactable) {
+                    button1.GetComponentInChildren<TMP_Text>().SetText("Confirm password\n({0}g)", GetUserNameCost(loginField.text));
+                }
+                break;
+            case 5:
+                ShowMessage(string.Format("Go forth, {0} and fullfill your destiny... after these messages.", username));
+                break;
+            case 6:
+                ShowMessage(string.Format("Your AdHero Premium trial has ended", username));
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void HandleInput() {
+        if (welcomeComplete) {
+            return;
+        }
+        switch (welcomeIndex) {
+            case 0:
+                ShowTitle();
+                OnProceed();
+                break;
+            case 1:
+                OnProceedWithAd(AdsManager.AdType.RewardedInterstitial);
+                break;
+            case 2:
+                OnProceed();
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                OnProceedWithAd(AdsManager.AdType.Interstitial);
+                break;
+            case 6:
+                Invoke("MuteAll", 1.0f);
+                Invoke("StartMainGame", 5f);
+                break;
+
+        }
+    }
+
+    //notice case + 1 is the end of welcomeIndex;
+    public void OnAdCompleted(AdsManager.AdType adType) {
+        switch (welcomeIndex) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+            case 4:
+                switch (adType) {
+                    case AdsManager.AdType.Interstitial:
+                        break;
+                    case AdsManager.AdType.Rewarded:
+                        AddCoins(20);
+                        break;
+                    case AdsManager.AdType.RewardedInterstitial:
+                        AddCoins(50);
+                        break;
+                }
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+        }
+    }
+
+    private void ShowTitle() {
+        titlePanel.SetActive(true);
+        messagePanel.SetActive(false);
+        loginPanel.SetActive(false);
+    }
+
+    private void ShowMessage(string msg) {
+        titlePanel.SetActive(false);
+        messagePanel.SetActive(true);
+        loginPanel.SetActive(false);
+
+        messagePanel.GetComponentInChildren<TMP_Text>().SetText(msg);
+    }
+
+    private void ShowLogin(string msg) {
+        titlePanel.SetActive(false);
+        messagePanel.SetActive(false);
+        loginPanel.SetActive(true);
+
+        loginPanel.GetComponentInChildren<TMP_Text>().SetText(msg);
+    }
+
+    public void OnProceedWithAd(AdsManager.AdType adType) {
+        adsManager.ShowAd(adType);
+        OnProceed();
+    }
+
+
+    public void OnProceed() {
+        welcomeIndex += 1;
+    }
+
+    public void OnButton1() {
+        if (!loginField.interactable) {
+            AddCoins(-5);
+            UnlockUsername();
+        } else {
+            AddCoins(-1 * GetUserNameCost(loginField.text));
+            username = loginField.text;
+            loginField.text = "";
+            OnProceed();
+        }
+    }
+
+    public void OnButton2() {
+        adsManager.ShowAd(AdsManager.AdType.Rewarded);
+    }
+    private int GetUserNameCost(string name) {
+        return name.Length * 10;
+    }
+
+
+    public void AddCoins(int c) {
+        int startCoins = coins;
+        coins += c;
+        StartCoroutine(DisplayCoins(startCoins));
+    }
+
+    public IEnumerator DisplayCoins(int startCoins) {
+        int jump = (coins - startCoins) / 10;
+
+        float totalTime = 0;
+        float duration = 1f;
+        int showCoins = startCoins;
+        while (showCoins != coins) {
+            showCoins = (int)Mathf.Lerp(startCoins, coins, totalTime / duration);
+            coinsText.GetComponent<TMP_Text>().SetText(showCoins.ToString());
+            totalTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public void UnlockUsername() {
+        loginField.interactable = true;
+    }
+
+    private void MuteAll() {
+        GetComponent<AudioSource>().mute = true;
+        Invoke("MuteBG", 2f);
+    }
+
+    private void MuteBG() {
+        var bwBG = Resources.Load<Sprite>("Morning-BW");
+        messagePanel.GetComponentInParent<Image>().sprite = bwBG;
+    }
+}
---
+++ Assets/MainGame.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+using System.Xml.Serialization;
+using UnityEngine.UI;
+
+public class MainGame : MonoBehaviour {
+
+    private AdsManager adsManager;
+
+    [SerializeField] GameObject mainPanel;
+    [SerializeField] GameObject earnCoinsPanel;
+    [SerializeField] GameObject accountPanel;
+    [SerializeField] GameObject levelUpPanel;
+    //[SerializeField] GameObject messagePanel;
+
+    [SerializeField] GameObject coinsText;
+
+    private int coins = 50;
+    private string username = "";
+    private int level = 1;
+
+    private bool soundUnlocked = false;
+    private bool colorUnlocked = false;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    public void InitMainGame(string username, int coin) {
+        adsManager = GetComponentInParent<AdsManager>();
+        adsManager.SetAdCompleteHandler(OnAdCompleted);
+
+        SetUsername(username);
+        SetCoins(coin);
+        coinsText.GetComponent<TMP_Text>().SetText(coins.ToString());
+
+        NavMainMenu();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began) {
+                HandleInput();
+            }
+        } else if (Input.GetMouseButtonDown(0)) {
+            HandleInput();
+        }
+
+    }
+
+    private void OnAdCompleted(AdsManager.AdType adType) {
+        if (adType == AdsManager.AdType.Rewarded) {
+            AddCoins(20);
+        }
+    }
+
+    public void HandleInput() {
+        //if (messagePanel.activeSelf) {
+            //messagePanel.SetActive(false);
+        //}
+    }
+    private void NavReset() {
+        mainPanel.SetActive(false);
+        accountPanel.SetActive(false);
+        //messagePanel.SetActive(false);
+        levelUpPanel.SetActive(false);
+        earnCoinsPanel.SetActive(false);
+    }
+
+    public void NavMainMenu() {
+        NavReset();
+        mainPanel.SetActive(true);
+    }
+
+    public void NavEarnCoins() {
+        NavReset();
+        earnCoinsPanel.SetActive(true);
+    }
+
+    public void NavAccount() {
+        NavReset();
+        accountPanel.SetActive(true);
+    }
+
+    public void NavPremium() {
+        adsManager.ShowAd(AdsManager.AdType.Rewarded);
+    }
+
+    public void OnEarnCoins() {
+        adsManager.ShowAd(AdsManager.AdType.Rewarded);
+    }
+
+    public void NavLevelUp() {
+        NavReset();
+        levelUpPanel.SetActive(true);
+    }
+
+    /*
+    private void NavLogin(string msg, buttonText, ) {
+        titlePanel.SetActive(false);
+        messagePanel.SetActive(false);
+        loginPanel.SetActive(true);
+
+        loginPanel.GetComponentInChildren<TMP_Text>().SetText(msg);
+    }
+    */
+
+
+    public void OnLevelUp() {
+        level += 1;
+        AddCoins(-50);
+        levelUpPanel.GetComponentInChildren<TMP_Text>().SetText("Your Hero Level\n{0}", level);
+    }
+
+    public void AddCoins(int c) {
+        int startCoins = coins;
+        coins += c;
+        StartCoroutine(DisplayCoins(startCoins));
+    }
+
+    public IEnumerator DisplayCoins(int startCoins) {
+        int jump = (coins - startCoins) / 10;
+
+        float totalTime = 0;
+        float duration = 1f;
+        int showCoins = startCoins;
+        while (showCoins != coins) {
+            showCoins = (int)Mathf.Lerp(startCoins, coins, totalTime / duration);
+            coinsText.GetComponent<TMP_Text>().SetText(showCoins.ToString());
+            totalTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public void SetCoins(int c) {
+        coins = c;
+    }
+
+    public void SetUsername(string u) {
+        username = u;
+    }
+
+    public void OnUnlockColor() {
+        //https://www.vectorstock.com/royalty-free-vector/landscape-at-morning-for-game-background-vector-14966453
+
+        AddCoins(-200);
+        colorUnlocked = !colorUnlocked;
+        ToggleColor();
+    }
+
+    private void ToggleColor() {
+
+        Sprite bg;
+        if (colorUnlocked) {
+            bg =  Resources.Load<Sprite>("Morning");
+        } else {
+            bg = Resources.Load<Sprite>("Morning-BW");
+        }
+
+        GameObject[] panels = new GameObject[] {mainPanel, earnCoinsPanel, accountPanel, levelUpPanel};
+        foreach (GameObject p in panels) {
+            p.GetComponent<Image>().sprite = bg;
+        }
+
+}
+
+    public void OnUnlockSound() {
+        AddCoins(-200);
+        ToggleSound();
+    }
+
+    private void ToggleSound() {
+        AudioSource bg = GetComponentInParent<AudioSource>();
+        bg.mute = !bg.mute;
+    }
+
+    //TODO screen transitions: https://docs.unity3d.com/Packages/com.unity.ugui@1.0/manual/HOWTO-UIScreenTransition.html
+}
